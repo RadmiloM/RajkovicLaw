d935f77bf999c9715dd64afb3cab4a55db720171
RadmiloM <markovicradmilo35@gmail.com>
2025-04-05 20:33:38 +0200

    added translations for contact details, commented unused text

--- contact.js
+++ contact.js
@@ -1,6 +1,33 @@
-console.log('connected to the app js file')
+const mobile = document.querySelector('.phone-text');
+const email = document.querySelector('.email-text-style');
 const back = document.querySelector('.go-back');
-console.log(back);
 back.addEventListener('click', () => {
     history.back();
 })
+
+const language = localStorage.getItem('language') || 'en';
+
+updateContactDetails();
+function updateContactDetails() {
+    switch(language) {
+        case 'en':
+            mobile.innerHTML = '&nbsp Phone';
+            email.innerHTML = '&nbsp Email';
+            return;
+        case 'ru':
+            mobile.innerHTML = '&nbsp Телефон';
+            email.innerHTML = '&nbsp Эл. почта';
+            return;
+        case 'fr': 
+            mobile.innerHTML = '&nbsp Téléphone';
+            email.innerHTML = '&nbsp Email';
+            return;
+        case 'srb':
+            mobile.innerHTML = '&nbsp Telefon';
+            email.innerHTML = '&nbsp E-pošta';
+            return;
+        default:
+            mobile.innerHTML = '&nbsp Phone';
+            email.innerHTML = '&nbsp Email';
+    }
+}
--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const russianFlag = document.querySelector('.russian');
 const englandFlag = document.querySelector('.great-britain');
 const serbianFlag = document.querySelector('.serbian');
 const franceFlag = document.querySelector('.france');
-const imageText = document.querySelector('.image-text');
+// const imageText = document.querySelector('.image-text');
+// console.log(imageText);
 
 
 russianFlag.addEventListener('click', () => {
+    localStorage.setItem('language','ru');
     const about = document.querySelector('.about');
     const services = document.querySelector('.services');
     const contact = document.querySelector('.contact');
@@ -13,14 +15,15 @@ russianFlag.addEventListener('click', () => {
     about.textContent = ' О нас'
     services.textContent = 'Услуги';
     contact.textContent = 'Контакты';
-    imageText.textContent = `В Rajkovic Law мы являемся защитниками справедливости,гарантируя,
-    что каждый человек и бизнес получат необходимую юридическую поддержку. 
-    Независимо от того, 
-    сталкиваетесь ли вы с сложными юридическими проблемами или нуждаетесь в консультации по личным вопросам, 
-    мы предоставляем четкое, эффективное и сострадательное представительство.`;
+    // imageText.textContent = `В Rajkovic Law мы являемся защитниками справедливости,гарантируя,
+    // что каждый человек и бизнес получат необходимую юридическую поддержку. 
+    // Независимо от того, 
+    // сталкиваетесь ли вы с сложными юридическими проблемами или нуждаетесь в консультации по личным вопросам, 
+    // мы предоставляем четкое, эффективное и сострадательное представительство.`;
 });
 
 englandFlag.addEventListener('click', () => {
+    localStorage.setItem('language','en');
     const about = document.querySelector('.about');
     const services = document.querySelector('.services');
     const contact = document.querySelector('.contact');
@@ -28,14 +31,15 @@ englandFlag.addEventListener('click', () => {
     about.textContent = 'About'
     services.textContent = 'Services';
     contact.textContent = 'Contact';
-    imageText.textContent =`At Rajkovic Law, we stand as the advocates of justice, 
-                ensuring that every individual and business receives the legal support they deserve. 
-                Whether you are facing complex legal challenges or need guidance on a personal matter, 
-                we provide clear, effective, and compassionate representation.`
+    // imageText.textContent =`At Rajkovic Law, we stand as the advocates of justice, 
+    //             ensuring that every individual and business receives the legal support they deserve. 
+    //             Whether you are facing complex legal challenges or need guidance on a personal matter, 
+    //             we provide clear, effective, and compassionate representation.`
 
 });
 
 franceFlag.addEventListener('click', () => {
+    localStorage.setItem('language','fr');
     const about = document.querySelector('.about');
     const services = document.querySelector('.services');
     const contact = document.querySelector('.contact');
@@ -43,13 +47,14 @@ franceFlag.addEventListener('click', () => {
     about.textContent = 'À propos'
     services.textContent = 'Services';
     contact.textContent = 'Contact';
-    imageText.textContent =`Chez Rajkovic Law, nous sommes les défenseurs de la justice, 
-    veillant à ce que chaque individu et chaque entreprise reçoive le soutien juridique qu'ils méritent. 
-    Que vous soyez confronté à des défis juridiques complexes ou que vous ayez besoin de conseils pour une affaire personnelle, 
-    nous offrons une représentation claire, efficace et compatissante.`;
+    // imageText.textContent =`Chez Rajkovic Law, nous sommes les défenseurs de la justice, 
+    // veillant à ce que chaque individu et chaque entreprise reçoive le soutien juridique qu'ils méritent. 
+    // Que vous soyez confronté à des défis juridiques complexes ou que vous ayez besoin de conseils pour une affaire personnelle, 
+    // nous offrons une représentation claire, efficace et compatissante.`;
 })
 
 serbianFlag.addEventListener('click', () => {
+    localStorage.setItem('language','srb');
     const about = document.querySelector('.about');
     const services = document.querySelector('.services');
     const contact = document.querySelector('.contact');
@@ -57,8 +62,10 @@ serbianFlag.addEventListener('click', () => {
     about.textContent = 'O nama'
     services.textContent = 'Usluge';
     contact.textContent = 'Kontakt';
-    imageText.textContent =`U Rajkovic Law, 
-    mi smo zagovornici pravde, osiguravajući da svaka osoba i preduzetnik dobiju pravnu podršku koju zaslužuju. 
-    Bilo da se suočavate sa složenim pravnim izazovima ili vam je potrebna pomoć u ličnim stvarima, pružamo jasno, 
-    efikasno i saosećajno zastupanje.`;
+    // imageText.textContent =`U Rajkovic Law, 
+    // mi smo zagovornici pravde, osiguravajući da svaka osoba i preduzetnik dobiju pravnu podršku koju zaslužuju. 
+    // Bilo da se suočavate sa složenim pravnim izazovima ili vam je potrebna pomoć u ličnim stvarima, pružamo jasno, 
+    // efikasno i saosećajno zastupanje.`;
 })
+
+localStorage.removeItem('language');
